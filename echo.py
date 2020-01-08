6181454beb2af5778041c7802ab1f97375c2a6f9
Jack Arthur Harrhy <me@jackharrhy.com>
2020-01-07 23:55:32 -0330

    gracefully exit via keyboard interrupt

--- echo.py
+++ echo.py
@@ -7,5 +7,8 @@ dotenv.load_dotenv()
 discord_webhook_url = os.environ["ECHO_DISCORD_WEBHOOK_URL"]
 hook = Webhook(discord_webhook_url)
 
-while True:
-	hook.send(input("echo > "))
+try:
+    while True:
+        hook.send(input("echo > "))
+except KeyboardInterrupt:
+    pass
